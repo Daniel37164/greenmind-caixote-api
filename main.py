740931969b521050152120378581ecf3f3166cdf
Daniel37164 <166503423+Daniel37164@users.noreply.github.com>
2026-04-20 14:20:08 +0000

    corrige cors

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 🔥 ADICIONA ISTO (CORS FIX)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # permite qualquer frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
